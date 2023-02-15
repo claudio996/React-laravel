6abe188ef03c510410a90b48041862b2ee008b15
Claudio996 <claudioantonio.retamal@gmail.com>
2023-02-15 01:39:25 -0300

    Working in component jsx-router in render views INDEX and NOT-FOUND

--- resources/js/components/router/Router.jsx
+++ resources/js/components/router/Router.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import { Routes, route } from 'react-router-dom';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import NotFound from '../NotFound'
-import Index from '../products'
-
-
+import NotFound from "../NotFound";
+import Index from "../products";
 
 const Router = () => {
-  return (
-    <div>Router</div>
-  )
-}
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/*" element= {<NotFound/>}></Route>
+            </Routes>
+        </div>
+    );
+};
 
-export default Router
+export default Router;
