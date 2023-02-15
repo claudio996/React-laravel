8f579549fcd43052817e2cd3b16343d5c4f5eac5
Claudio996 <claudioantonio.retamal@gmail.com>
2023-02-15 01:28:33 -0300

    Create route for components

---
+++ resources/js/components/router/Router.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Routes, route } from 'react-router-dom';
+
+import NotFound from '../NotFound'
+import Index from '../products'
+
+
+
+const Router = () => {
+  return (
+    <div>Router</div>
+  )
+}
+
+export default Router
