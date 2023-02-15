8fd76c4b4ec8ad3d9c21ef0d2a628f7e766540ba
Claudio996 <claudioantonio.retamal@gmail.com>
2023-02-15 14:27:06 -0300

    Working in app.css maqueting hTML

--- resources/js/components/products/Index.jsx
+++ resources/js/components/products/Index.jsx
@@ -1,10 +1,48 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Index = () => {
+    const newProduct = () => {
+        useNavigate('/product/new')
+    };
+
     return (
         <>
-            <div>
-                <h1>Hola</h1>
+            <div className="container">
+                <div className="products__list">
+                    <div className="titlebar">
+                        <div className="titlebar_item">
+                            <h1>Products</h1>
+                        </div>
+                        <div className="titlebar_item">
+                            <div className="btn">add Product</div>
+                        </div>
+                    </div>
+                    <div className="table">
+                        <div className="list_header">
+                            <p>Image</p>
+                            <p>Product</p>
+                            <p>type</p>
+                            <p>Inventory</p>
+                            <p>actions</p>
+                        </div>
+                        <div className="list_items">
+                            <img src="" alt="" height="40px" />
+                            <a>product name</a>
+                            <p>Category</p>
+                            <p>50</p>
+                            <div>
+                                <button className="btn-icon success">
+                                    <i className="fas fa-pencil-alt"></i>
+                                </button>
+
+                                <button className="btn-icon danger">
+                                    <i className="fas fa-trash-alt"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
