5599bcbf38cea3dc8dc7d3df9a9e97777c3202ae
Claudio996 <claudioantonio.retamal@gmail.com>
2023-02-15 01:12:12 -0300

    Create componet parent Products

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,1 +1,6 @@
-import './bootstrap';
+import React from "react";
+import "./bootstrap";
+import  ReactDOM  from "react-dom/client";
+import App from "./components/App";
+
+ReactDOM.createRoot(document.getElementById('app')).render(< App />);
